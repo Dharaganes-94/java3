cf010d62ed32e6dce6f41248bc00bfe1a013d2b3
dharani <dharani.gsd@gmail.com>
2023-09-27 21:56:28 +0530

    Added the new files

---
+++ ArrayListEx.java
@@ -0,0 +1,40 @@
+package Advanced;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//first way to store heteregenous data
+
+		ArrayList al = new ArrayList();
+		
+		al.add("java");
+		al.add(100);
+		al.add(true);
+		al.add('c');
+		al.add(100);
+		al.add("");
+		al.add(null);
+		System.out.println(al);
+		
+		//second way -- to create arraylist with homogenous data
+		
+		ArrayList<Integer> al1 = new ArrayList<Integer>();
+		ArrayList<String> al2 = new ArrayList<String>();
+		
+		al1.add(102);
+		al2.add(null);
+		System.out.println(al2);
+		
+		//Third way
+        
+		List al4=new ArrayList();
+
+	}
+
+}
